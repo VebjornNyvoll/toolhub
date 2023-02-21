8666c95e9c34759dddacf7480c176631d4648a26
idunskretting <idunas@stud.ntnu.no>
2023-02-21 12:51:26 +0100

    Lagde Seacrhbar front end Det avslutter issue-15

--- src/components/searchbar/Searchbar.tsx
+++ src/components/searchbar/Searchbar.tsx
@@ -12,7 +12,7 @@ const Searchbar = () => {
     <div className=" z-10 my-4 flex max-w-md gap-4 rounded-lg bg-white p-5 text-lg text-stone-900 shadow-md">
       <MagnifyingGlassIcon className="h-7 w-7" strokeWidth={1.5} />
       <input
-        className="w-full "
+        className="w-full outline-none"
         type="text"
         placeholder="Hvilke verktøy ser du etter?"
         onChange={handleChange}
